9b09a2fa9afd3c00e2999c33fb6622d2e34bcf58
Drew Morton <drew.morton94@gmail.com>
2018-06-19 22:35:34 -0400

    Update Comments & Confidence
    Confidence is now applied for all abilities

---
+++ Tests/ReadyFlinchDodge.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Random;
+
+Random RNG = new Random();
+
+public class ReadyDodgeYell
+{
+
+    public startGame()
+    {
+        String name1;
+        String name2;
+        int health = 100;
+
+        String[] actions = ["Attack", "Dodge", "Yell"];
+
+    }
+
+    static int Main(string[] args)
+    {
+        Player p1 = new Player();
+        Player p2 = new Player();
+
+        startgame();
+
+        Console.WriteLine("***** My First C# App *****");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine();
+        Console.ReadLine();
+
+        return 0;
+    }
+}
+
+public class Player
+{
+    public string name;
+    public int health = 100;
+    String[] actions = ["Attack", "Dodge", "Yell"];
+    Boolean speed = false;
+    Boolean confident = false;
+    public Player(string name, int health, String[] actions, Boolean speed, Boolean confident)
+    {
+        this.name = name;
+        this.health = health;
+        this.actions = actions;
+        this.speed = speed;
+        this.confident = confident;
+    }
+}
--- game.py
+++ game.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 '''
 Created by Drew Morton & Nick McCarty
-Last Editted: 05/14/18
 
 This game will be a mobile fighting game.
 Either:
 	1 v 1
 	2 v 2
 
-Turn based but 3 abilities at a time instead of 1
+Turn based but 3 abilities at a time instead of 1.
 
 Comment Key:
 TODO - action item needs to be completed for some reason
@@ -18,30 +17,23 @@ NOTE - typically used before code that is only for testing purposes but may not 
 '''
 import random
 
-# Start Game method 
+# Start Game method
+# This method will gather the names of the two players and initialize each of the players
 def start_game():
+	print ("Welcome to Ready! Flinch!! Dodge!!!\n")
 	name1 = input("What is first player's name? \n")
 	name2 = input("\nWhat is second player's name? \n")
-	health = 100
-
 	
 	# Actions will be standardized and we may introduce swapping abilities in the future
-	actions = ["Attack", "Dodge", "Yell"]
-
-	p1 = make_player(name1, health, actions, Player.speed, Player.confident)
-	p2 = make_player(name2, health, actions, Player.speed, Player.confident)
+	p1 = make_player(name1, Player.health, Player.actions, Player.speed, Player.confident)
+	p2 = make_player(name2, Player.health, Player.actions, Player.speed, Player.confident)
 	
-	print("\n########################################")
-	print_player(p1)
-	print("########################################")
-	print_player(p2)
-	print("########################################\n")
 	begin_fight(p1, p2)
-
 
 # p1 = player 1; p2 = player 2
 def begin_fight(p1, p2):
-	print("Combat has begun and will be played in rounds until there is only one team left.")
+	print("----------------------------------------------------------------------------")
+	print("Combat has begun and will be played in rounds until there is only one team left.\n")
 	gameRound = 1
 
 	#TODO: Review best place for checking confidence_list may need to be moved to another function.
@@ -57,34 +49,27 @@ def begin_fight(p1, p2):
 		p2.speed = True
 
 	if p1.speed == True:
-		print("Player {} begins the fight with a speedy advantage".format(p1.name))
+		print("Player {} begins the fight with a speedy advantage.".format(p1.name))
 	else:
-		print("Player {} begins the fight with a speedy advantage".format(p2.name))
+		print("Player {} begins the fight with a speedy advantage.".format(p2.name))
 
 	# While both players are alive with 1 health or more.
-	# --Exception: If a player dies mid round
+	# --Exception: If a player dies mid round must complete the rest of the rounds
 	while p1.health >= 1 and p2.health >= 1:
-		print("Begin round:{}".format(gameRound))
-		print("--------------------------------------")
+		print("\nBegin round:{}".format(gameRound))
+		print("----------------------------------------------------------------------------")
 
 		if gameRound > 1:
 			print("\n<><><><><><><><><><><<><><><><><><><><><><><><><><><><><><><><>")
 			print("Last Round: \nPlayer 1 chose {} \nPlayer 2 chose {}".format(p1Actions, p2Actions))
 			print("<><><><><><><><><><><<><><><><><><><><><><><><><><><><><><><><>\n")
 
-		# TODO: COMPLETE make separate function to assign actions per round
 		p1Actions = choose_actions(p1)
 		p2Actions = choose_actions(p2)
 
-		# Checks whether a player is confident
-		if str(p1Actions) == str(confidence_list):
-			p1.confident = True
-		else:
-			p1.confident = False
-		if str(p2Actions) == str(confidence_list):
-			p2.confident = True
-		else:
-			p2.confidence =  False
+		# Checks whether a player is too confident to do nothing
+       	p1.confident = is_confident(p1Actions)
+       	p2.confident = is_confident(p2Actions)
 
 		p1AttackList = []
 		p2AttackList = []
@@ -95,7 +80,7 @@ def begin_fight(p1, p2):
 		print("\n{} will use {}".format(p1.name,', '.join(p1AttackList)))
 		print("\n{} will use {}\n".format(p2.name,', '.join(p2AttackList)))
 
-		compare_actions2(p1, p1Actions, p2, p2Actions)
+		compare_actions(p1, p1Actions, p2, p2Actions)
 
 		print("Players health 1:{} 2:{}".format(p1.health, p2.health))
 
@@ -113,8 +98,8 @@ def begin_fight(p1, p2):
 		print(x)
 
 # choose_actions - Prompts player to choose their 3 actions
-# TODO: Allow user to click a button, enter string, and/or all 3 actions on one line. 
-# --Exception: Allows user to pick non-# but only #'s in the list
+# TODO: Allow user to enter all 3 actions on one line. 
+# --Exception: Allows user to pick non-# but only #'s in the list will work
 def choose_actions(player):
 
 	# Re-format action list in format ACTION:EXPECTED INPUT
@@ -132,22 +117,31 @@ def choose_actions(player):
 		response = valid_input(temp, player.actions, player.confident)
 		attack.append(response)
 	print(attack)
-
 	return attack
 
 # valid_input - determines if the response given is a valid option in the given list.
-def valid_input(response, l, confident):
+def valid_input(response, action_list, confident):
 	invalid = True
 	while invalid == True:
-		if response < 0 or response > (len(l) - 1):
+		if response < 0 or response > (len(action_list) - 1):
 			print("You have chosen an invalid action.")
+			response = int(input("Action : "))
+		elif confident == True and response == 0:
+			print("Your opponent is expecting another hit! Choose another action.")
 			response = int(input("Action : "))
 		elif confident == True and response == 1:
 			print("You are too confident to defend! Choose another action.")
 			response = int(input("Action : "))
+		elif confident == True and response == 3:
+			print("Your throat hurts from yelling! Choose another action.")
+			response = int(input("Action : "))	
 		else:
 			invalid = False
 	return response
+
+# Checks if all items in the list are the same
+def is_confident(actions):
+	return len(set(actions)) <= 1
 
 # compare_actions - Determines who wins each action of a round
 # Using the below Rock Paper Scissors logic, for each loss the player loses 25 health. 
@@ -155,22 +149,21 @@ def valid_input(response, l, confident):
 # If the difference between the two actions is 1 then action with greater value won
 # --Exception: to the above: beginning w/ end scenario not covered. EX: Rock & Scissors
 #   would result in nothing happening.
-# Compare Actions 2 follows the logic discussed in meeting notes
-def compare_actions2(p1, attack1, p2, attack2):
+# Compare Actions follows the logic discussed in meeting notes
+def compare_actions(p1, attack1, p2, attack2):
 	# For loop for the range of the list of attacks assuming both are the same length.
 	for x in range((len(attack1))):
 		if attack1[x] == attack2[x]:
-			if attack1[x] == 0: # Attack V Attack
+			if attack1[x] == 0: # Attack V Attack = quicker one deals damage
 				if p1.speed:
 					p2.health -= 20
 					print("{} quickly attacks {} before they can move!".format(p1.name,p2.name)) 
 				else:
 					p1.health -= 20
 					print("{} quickly attacks {} before they can move!".format(p2.name,p1.name)) 
-			elif attack1[x] == 1: # Dodge V Dodge
+			elif attack1[x] == 1: # Dodge V Dodge = nothing happens
 				print("Both players stare at each other waiting for the other to move!")
-			else: # Yell & Yell
-				# both take half damage
+			else: # Yell & Yell = both take half damage
 				print("Both players yell and hurt one another's ears!")
 				p1.health -= 10 
 				p2.health -= 10 
@@ -199,7 +192,6 @@ def compare_actions2(p1, attack1, p2, attack2):
 				p1.health -= 20
 				print("{} yells at staring {} hurting their ears!".format(p2.name,p1.name)) 
 
-
 # Player class
 # Unsure if naming convention is correct. I think it needs an underscore in front of it.
 class Player(object):
@@ -226,8 +218,5 @@ def print_player(object):
 	print("\n{} is a player".format(object.name))
 	print("Current health is {}".format(object.health))
 	print("All possible actions for current player are {}.\n".format(', '.join(object.actions)))
-	#print("Does the player have speed boost? {} ".format(object.speed))
-
-print ("Welcome to Ready! Flinch!! Dodge!!!\n")
 
 start_game()
